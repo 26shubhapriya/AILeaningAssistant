7b6d51eddffd76f47fe0325febe8a74d628ebd6c
26shubhapriya <shubhapriya1998@gmail.com>
2026-07-02 19:22:28 +0530

    chnages made while deployement

--- backend/controllers/documentController.js
+++ backend/controllers/documentController.js
@@ -1,7 +1,7 @@
 import Document from "../models/Document.js";
 import Flashcard from "../models/Flashcard.js";
 import Quiz from "../models/Quiz.js";
-import { extractTextFromPDF } from "../utils/pdfParser.js";
+import { extractTextFromPDF } from "../utils/pdfparser.js";
 import { chunkText } from "../utils/textChunker.js";
 import fs from "fs/promises";
 import mongoose from "mongoose";
